43576083eb68e362839295fd0c2a92005924d19a
msearles25 <skaman4459@gmail.com>
2019-10-17 17:31:33 -0400

    lambda classes done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -34,13 +34,13 @@ class Student extends Person{
         this.favSubjects = attr.favSubjects
     }
     listSubjects() {
-        return ``;
+        return `First: ${this.favSubjects[0]}, Second: ${this.favSubjects[1]}, Third: ${this.favSubjects[2]}`;
     }
-    PRAssignment(subject) {
-        return `${student.name} has submitted a PR for ${subject}`;
+    prAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
     }
     sprintChallenge(subject) {
-        return `${student.name} has begun sprint challenge on ${subject}`;
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 
@@ -50,7 +50,86 @@ class ProjectManager extends Instructor {
         this.gradClassName = attr.gradClassName,
         this.favInstructor = attr.favInstructor
     }
-    standUp(name, channel) {
-        return `${name} announces to ${channel}, @channel standy times!​​​​​`;
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
+    }
+    debugsCode(subject, student) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
+
+const britt = new Instructor({
+    name: 'Britt',
+    location: 'Canada',
+    age: 32,
+    favLanguage: 'JavaScript',
+    specialty: 'Teaching',
+    catchPhrase: 'Awesome!'
+});
+
+const josh = new Instructor({
+    name: 'Josh',
+    location: 'USA',
+    age: 35,
+    favLanguage: 'Pythont',
+    specialty: 'Teaching',
+    catchPhrase: 'Isn\'t that cool?!'
+});
+
+const kevin = new ProjectManager ({
+    name: 'Kevin',
+    location: 'Wisconsin',
+    age: 30,
+    favLanguage: 'React',
+    specialty: 'Being a great Team Lead',
+    catchPhrase: 'I do\'t want to take up all of your time tonight!',
+    gradClassName: 'Web 17',
+    favInstructor: 'Josh Knell'
+});
+
+const faye = new ProjectManager ({
+    name: 'Faye',
+    location: 'USA',
+    age: 29,
+    favLanguage: 'HTML',
+    specialty: 'breakout rooms',
+    catchPhrase: 'Good Job!',
+    gradClassName: 'Web 18',
+    favInstructor: 'Josh Knell'
+});
+
+const jessica = new Student ({
+    name: 'Jessica',
+    location: 'USA',
+    age: 25,
+    previousBackground: 'Cashier',
+    className: 'Web 25',
+    favSubjects: [
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ]
+})
+
+const joe = new Student ({
+    name: 'Joe',
+    location: 'USA',
+    age: 18,
+    previousBackground: 'Highschool',
+    className: 'Web 26',
+    favSubjects: [
+        'HTML',
+        'CSS',
+        'JavaScript'
+    ]
+})
+
+
+console.log(britt.speak());
+console.log(britt.demo('JavaScript'));
+console.log(josh.grade(jessica, 'JavaScript'));
+console.log(jessica.listSubjects());
+console.log(jessica.prAssignment('JavaScript'));
+console.log(joe.sprintChallenge('React'));
+console.log(kevin.standUp('WEB25'));
+console.log(faye.debugsCode('JavaScript', jessica));
