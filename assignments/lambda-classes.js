e6d57ab2a6d57c80642fb21b70e3480caaca4d15
msearles25 <skaman4459@gmail.com>
2019-10-17 16:26:00 -0400

    backup of lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,56 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attr) {
+        this.name = attr.name,
+        this.age = attr.age,
+        this.location = attr.location
+    }
+    speak() {
+       return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(attr) {
+        super(attr);
+        this.specialty = attr.specialty,
+        this.favLanguage = attr.favLanguage,
+        this.catchPhrase = attr.catchPhrase
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Student extends Person{
+    constructor(attr) {
+        super(attr);
+        this.previousBackground = attr.previousBackground,
+        this.className = attr.className,
+        this.favSubjects = attr.favSubjects
+    }
+    listSubjects() {
+        return ``;
+    }
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(attr) {
+        super(attr);
+        this.gradClassName = attr.gradClassName,
+        this.favInstructor = attr.favInstructor
+    }
+    standUp(name, channel) {
+        return `${name} announces to ${channel}, @channel standy times!​​​​​`;
+    }
+}
